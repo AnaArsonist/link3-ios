f1225df33ef255156afcda59432afe6b830ffee6
Ana <113212503+AnaArsonist@users.noreply.github.com>
2023-07-22 22:37:57 +0200

    Initial Commit

---
+++ link3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  link3
+//
+//  Created by Ana Howard on 22/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ link3/link3App.swift
@@ -0,0 +1,17 @@
+//
+//  link3App.swift
+//  link3
+//
+//  Created by Ana Howard on 22/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct link3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
