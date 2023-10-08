72b6d63352acb6d717c5c5428adebba0567c3778
Antony1060 <antoniostignjedec@gmail.com>
2023-10-08 13:34:32 +0200

    Small fixes

--- link3/Models/ENSNameViewModel.swift
+++ link3/Models/ENSNameViewModel.swift
@@ -14,17 +14,17 @@ public class ENSNameViewModel: ObservableObject {
     static let baseGQL = "query getNames($id: ID!) { account(id: $id) { domains(first: 1000) { name createdAt registration { expiryDate } } wrappedDomains(first: 1000) { domain { name createdAt registration { expiryDate } } } } }"
 
     func fetch(address: String) {
-        let json: [String: Any] = ["query": Test.baseGQL, "variables": ["id": address.lowercased()], "operationName": "getNames"]
+        let json: [String: Any] = ["query": ENSNameViewModel.baseGQL, "variables": ["id": address.lowercased()], "operationName": "getNames"]
 
         let networkRequest = apiManager.networkRequest(baseURL: "https://api.thegraph.com", endpoint: TheGraphEndpoints.ens, headers: ["Content-Type": "application/json"])
         // TODO: integrate in apiManager
-        request.httpBody = try! JSONSerialization.data(withJSONObject: json)
+        networkRequest.httpBody = try! JSONSerialization.data(withJSONObject: json)
 
-        apiManager.networkTask(request: networkRequest) { [weak self] (response: Result<TheGraphBaseResponseModel, Error>) in
+        apiManager.networkTask(request: networkRequest as URLRequest) { [weak self] (response: Result<TheGraphBaseResponseModel, Error>) in
             switch response {
             case .success(let data):
-                let ownedNames = data.data.account.domains.filter { !$0.name.hasSuffix("addr.reverse") }.map { Test.ensNameFromRawDomain($0, wrapped: false) }
-                let wrappedNames = data.data.account.wrappedDomains.map { $0.domain }.map { Test.ensNameFromRawDomain($0, wrapped: true) }
+                let ownedNames = data.data.account.domains.filter { !$0.name.hasSuffix("addr.reverse") }.map { ENSNameViewModel.ensNameFromRawDomain($0, wrapped: false) }
+                let wrappedNames = data.data.account.wrappedDomains.map { $0.domain }.map { ENSNameViewModel.ensNameFromRawDomain($0, wrapped: true) }
 
                 DispatchQueue.main.async {
                     self?.names = ownedNames + wrappedNames
@@ -38,7 +38,7 @@ public class ENSNameViewModel: ObservableObject {
     private static func ensNameFromRawDomain(_ domain: RawDomainModel, wrapped: Bool) -> ENSName {
         return ENSName(
             name: domain.name,
-            image_url: "https://metadata.ens.domains/mainnet/avatar/\(domain.name)",
+            imageUrl: "https://metadata.ens.domains/mainnet/avatar/\(domain.name)",
             createdAt: NSDate(timeIntervalSince1970: Double(domain.createdAt) ?? 0) as Date,
             expiresAt: domain.registration != nil ?
                             NSDate(timeIntervalSince1970: Double(domain.registration!.expiryDate) ?? 0) as Date :
